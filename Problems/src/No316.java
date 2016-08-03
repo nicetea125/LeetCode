5f62289cf02056041222ad0dca8bc3958194a5dc
Nicetea <nicetea125@gmail.com>
2016-08-03 22:01:10 +0800

    No316 : Accepted

--- Problems/src/No316.java
+++ Problems/src/No316.java
@@ -7,8 +7,32 @@ public class No316 {
 
     public static void main(String[] args)  {
 
-        System.out.print( getMoneyAmount(200) );
+        System.out.print( removeDuplicateLetters("cbacdcbc")  );
     }
 
-    
+    public static String removeDuplicateLetters(String s) {
+        int[] letterCnt = new int[36]; // a=10 ~ z=35
+
+        String result = "";
+        for(Character c : s.toCharArray()){
+            letterCnt[Character.getNumericValue(c)]++;
+        }
+
+        for(Character c : s.toCharArray()){
+            letterCnt[Character.getNumericValue(c)]--;
+            if(result.contains(c.toString())) continue;
+
+            while( result.length()!=0
+                    && result.charAt(result.length()-1) > c
+                    && letterCnt[Character.getNumericValue(result.charAt(result.length()-1))] > 0){
+
+                result = result.substring(0,result.length()-1);
+            }
+
+            result += c;
+        }
+        return result;
+    }
+
+
 }
