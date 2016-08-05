9a1a6e34eb061c72b975e9da135846163ab65f7c
Nicetea <nicetea125@gmail.com>
2016-08-05 19:22:01 +0800

    No312 : change to String as key

--- Problems/src/No312.java
+++ Problems/src/No312.java
@@ -3,11 +3,17 @@ import java.util.*;
 
 public class No312 {
 
+    //private static Map<String,Integer> map = new HashMap<String,Integer>();
     private static Map<ArrayList<Integer>,Integer> map = new HashMap<ArrayList<Integer>,Integer>();
 
     public static void main(String[] args)  {
+        long startTime = System.currentTimeMillis();
+
         int[] nums = {8,2,6,8,9,8,1,4,1,5,3,0,7,7,0,4,2};
-        System.out.print( maxCoins(nums) );
+        System.out.println( maxCoins(nums) );
+
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime + "ms");
     }
 
     public static int maxCoins(int[] nums) {
@@ -16,7 +22,8 @@ public class No312 {
         return getMaxCoin(numList);
     }
 
-    public static int getMaxCoin(ArrayList<Integer> numList){
+    private static int getMaxCoin(ArrayList<Integer> numList){
+        //String numString = convertToString(numList);
         if(map.get(numList) != null) return map.get(numList);
 
         int size = numList.size();
@@ -38,9 +45,21 @@ public class No312 {
         return coins;
     }
 
-    public static int getBurstCoin(ArrayList<Integer> numList, int i, int size){
+    private static int getBurstCoin(ArrayList<Integer> numList, int i, int size){
         int left  = (i==0) ? 1 : numList.get(i-1);
         int right = (i==size-1) ? 1 : numList.get(i+1);
         return left * numList.get(i) * right;
     }
+
+    /*
+    private static String convertToString(ArrayList<Integer> numbers) {
+        StringBuilder builder = new StringBuilder();
+        for (int number : numbers) {
+            builder.append(number);
+            builder.append(":");
+        }
+        builder.setLength(builder.length() - 1);
+        return builder.toString();
+    }
+    */
 }
